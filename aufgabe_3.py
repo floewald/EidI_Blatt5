f0db9ca75dd390f9f8be3fd173bd64979ad36cc7
fewald <uk029985@student.uni-kassel.de>
2020-12-10 14:05:38 +0100

    update task 3

--- aufgabe_3.py
+++ aufgabe_3.py
@@ -1,39 +1,22 @@
 # Carina Moitov
 # Arthur Flaum
 # Florian Ewald, 333 068 94
-# def add_up(tree):
-#     level   = 1
-#     branch  = []
-#     for m in range(0,3,2):
-#         if type(tree[m]) == tuple:
-#             tmp = add_up(tree[m])
-#             branch.append(tmp)
-#         else:
-#             branch.append(tree[m])
-
-#     print(branch)
-#     return tree[1] +sum(branch)
-
 def add_up(tree):
-    def add_up_aux(tree, level):
+    return add_up_aux(tree, 1)
+
+def add_up_aux(tree, level):
+    if type(tree) == tuple:
+        treesum = level*tree[1] + add_up_aux(tree[0], level+1) + add_up_aux(tree[2], level+1)
+        return treesum
+    elif type(tree) == int:
         return level*tree
-    level = 1
-    branch = 0
-    for m in range(0, 3, 2):
-        if type(tree[m]) == tuple:
-            level += 1
-            branch += add_up_aux(tree[1], level) + add_up(tree[m])
-        else:
-            branch+= add_up_aux(tree[1], 1)
-    print(branch)
-    return branch
-
-
 
 tree = ((111, -16, -26), 81, -7)
+tree = ((3,5,-26),4,((3,3,2),1,((-12,9,120),-11,7)))
 # tree = (-26, 81, -7)
 print(add_up(tree))
 a = 111 -16 -26 + 81 - 7
 # a = 3*111 -2*16 -3*26 + 81 - 2*7
 print(a)
-print("290")
+# print("290")
+print("547")
